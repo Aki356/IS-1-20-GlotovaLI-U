cbeef76739db1b082883164c71375e3607576d89
глотова любовь <lyuba.glotova@yandex.ru>
2022-12-02 17:59:38 +0500

    Закончила 1 задание.

--- IS-1-20-GlotovaLI-U/Accessories.cs
+++ IS-1-20-GlotovaLI-U/Accessories.cs
@@ -16,7 +16,7 @@ namespace IS_1_20_GlotovaLI_U
         {
             public double price;
             public int yearRelease;
-            T artic;
+            public T artic;
             public Accessory(double price, int yearR, T art)
             {
                 this.price = price;
@@ -25,15 +25,15 @@ namespace IS_1_20_GlotovaLI_U
             }
             public string Display()
             {
-                return "Артикул: " + artic + "\n" + price + " руб. " + yearRelease + " год.";
+                return $"Артикул: {artic}\nЦена: {price} руб.\n{yearRelease} год.";
             }
         }
-        class Hdd : Accessory<T>
+        class Hdd<T> : Accessory<T>
         {
             private double NumRevol { get; set; }
             private string Interfc { get; set; }
             private int Capacity { get; set; }
-            public Hdd(double nR, string i, int c, double price, int yearR):base(price, yearR)
+            public Hdd(double nR, string i, int c, double price, int yearR, T art):base(price, yearR, art)
             {
                 NumRevol = nR;
                 Interfc = i;
@@ -41,15 +41,15 @@ namespace IS_1_20_GlotovaLI_U
             }
             public new string Display()
             {
-                return "Кол-во оборотов: " + NumRevol + ", интерфейс: " + Interfc + ", объем жесткого диска: " + Capacity + ", цена: " + price + ", год выпуска: " + yearRelease;
+                return "Кол-во оборотов: " + NumRevol + ", интерфейс: " + Interfc + ", объем жесткого диска: " + Capacity + ", цена: " + price + " руб., год выпуска: " + yearRelease;
             }
         }
-        class VideoCard : Accessory
+        class VideoCard<T> : Accessory<T>
         {
             private double GpuFreq { get; set; }
             private string Manuf { get; set; }
             private int Capacity { get; set; }
-            public VideoCard(double gF, string m, int c, double price, int yR):base(price, yR)
+            public VideoCard(double gF, string m, int c, double price, int yR, T art):base(price, yR, art)
             {
                 GpuFreq = gF;
                 Manuf = m;
@@ -67,13 +67,41 @@ namespace IS_1_20_GlotovaLI_U
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Hdd hdd = new Hdd(Convert.ToDouble(textBox3.Text), textBox4.Text, Convert.ToInt32(textBox5.Text), Convert.ToDouble(textBox1.Text), Convert.ToInt32(textBox2.Text));
-            listBox1.Items.Add(hdd.Display());
+            try
+            {
+                Accessory<string> hdd = new Hdd<string>(Convert.ToDouble(textBox3.Text), textBox4.Text, Convert.ToInt32(textBox5.Text), Convert.ToDouble(textBox1.Text), Convert.ToInt32(textBox2.Text), textBox6.Text);
+                listBox1.Items.Add(hdd.Display());
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Возникла ошибка! Проверьте введенные данные.");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                Hdd<string> hdd = new Hdd<string>(Convert.ToDouble(textBox3.Text), textBox4.Text, Convert.ToInt32(textBox5.Text), Convert.ToDouble(textBox1.Text), Convert.ToInt32(textBox2.Text), textBox6.Text);
+                listBox1.Items.Add(hdd.Display());
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Возникла ошибка! Проверьте введенные данные.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VideoCard<string> vidC = new VideoCard<string>(Convert.ToDouble(textBox7.Text), textBox8.Text, Convert.ToInt32(textBox9.Text), Convert.ToDouble(textBox1.Text), Convert.ToInt32(textBox2.Text), textBox6.Text);
+                listBox1.Items.Add(vidC.Display());
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Возникла ошибка! Проверьте введенные данные.");
+            }
         }
     }
 }
